2f4ec9f82c714fe65dfe55c49f11446010b24139
Mark <markthompson_tla@yahoo.com>
2025-07-04 19:39:54 -0400

    forgot to add

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -2,7 +2,7 @@
 import SkillCategory from '../../components/SkillCategory';
 import jsIcon from '../../assets/javascript_logo.png';
 import pyIcon from '../../assets/python_logo.png';
-import htmlIcon from "../../assets/html5_logo.png";  // Replace with actual paths
+import htmlIcon from "../../assets/html5_logo.png";  
 import cssIcon from "../../assets/css3_logo.png";
 import reactIcon from "../../assets/react_logo.png";
 import awsIcon from "../../assets/aws_logo.png";
@@ -16,34 +16,33 @@ import tailwindIcon from "../../assets/tailwind_logo.png";
 
 export default function Skills() {
   const programmingLanguages = [
-    { name: 'Javascript', icon: jsIcon },
-    { name: 'Python', icon: pyIcon },
-    { name: 'HTML', icon: htmlIcon },
-    { name: 'CSS', icon: cssIcon },
-    { name: 'Java', icon: javaIcon },
-    { name: 'PHP', icon: phpIcon },
-    { name: 'Go', icon: goIcon },
-    { name: 'SQL', icon: sqlIcon },
-    // add more...
+    { name: 'Javascript', icon: jsIcon, glowColor: "#F7DF1E" }, 
+    { name: 'Python', icon: pyIcon, glowColor: "#3776AB" },          
+    { name: 'CSS', icon: cssIcon, glowColor: "#1572B6" },
+    { name: 'HTML', icon: htmlIcon, glowColor: "#E34F26" },       
+    { name: 'Java', icon: javaIcon, glowColor: "#007396" },     
+    { name: 'PHP', icon: phpIcon, glowColor: "#8892BF" },       
+    { name: 'Go', icon: goIcon, glowColor: "#00ADD8" },         
+    { name: 'SQL', icon: sqlIcon, glowColor: "#003B57" },       
   ];
+  
 
   const Tools = [
-    { name: 'Github', icon:githubIcon },
-    { name: 'Docker', icon: dockerIcon },
-    { name: 'AWS', icon: awsIcon },
-    // add more...
+    { name: 'Github', icon: githubIcon, glowColor: "#6e5494" },  
+    { name: 'Docker', icon: dockerIcon, glowColor: "#0db7ed" },  
+    { name: 'AWS', icon: awsIcon, glowColor: "#FF9900" },        
   ];
+  
 
   const Libraries = [
-    { name: 'React', icon: reactIcon },
-    { name: 'Tailwind CSS', icon: tailwindIcon },
-    // add more...
+    { name: 'React', icon: reactIcon, glowColor: "#61DAFB" },
+    { name: 'Tailwind CSS', icon: tailwindIcon,  glowColor: "#38BDF8" },
   ];
 
   return (
-    <div className="min-h-screen bg-black text-white px-8 py-16">
+    <div className="min-h-screen bg-black text-white px-8 pt-28 pb-10">
       <div className="max-w-5xl mx-auto px-4">
-        <h1 className="text-4xl font-bold text-center mt-8 mb-6">Skills</h1>
+        <h1 className="text-4xl font-bold text-center mb-10">Skills</h1>
 
           <SkillCategory title="Programming Languages" skills={programmingLanguages} />
           <SkillCategory title="Libraries" skills={Libraries} />
